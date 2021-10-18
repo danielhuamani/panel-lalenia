2ea327c9e07bc98d827c4da75546ce752f897629
danielhuamani <danielhuamani15@gmail.com>
2021-10-17 23:10:05 -0500

    removed router todo

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,12 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import VueMeta from 'vue-meta'
-
-import store from '@/state/store'
-import routes from './routes'
+import Vue from "vue"
+import VueRouter from "vue-router"
+import VueMeta from "vue-meta"
+import routes from "./routes"
 
 Vue.use(VueRouter)
 Vue.use(VueMeta, {
   // The component option name that vue-meta looks for meta info on.
-  keyName: 'page',
+  keyName: "page",
 })
 
 const router = new VueRouter({
@@ -17,7 +15,7 @@ const router = new VueRouter({
   // instead of routes with hashes (e.g. example.com/#/about).
   // This may require some server configuration in production:
   // https://router.vuejs.org/en/essentials/history-mode.html#example-server-configurations
-  mode: 'history',
+  mode: "history",
   // Simulate native-like scroll behavior when navigating to a new
   // route and using back/forward buttons.
   scrollBehavior(to, from, savedPosition) {
@@ -31,44 +29,33 @@ const router = new VueRouter({
 
 // Before each route evaluates...
 router.beforeEach((routeTo, routeFrom, next) => {
-  if (process.env.VUE_APP_DEFAULT_AUTH === "firebase") {
-    // Check if auth is required on this route
-    // (including nested routes).
-    const authRequired = routeTo.matched.some((route) => route.meta.authRequired)
+  // Check if auth is required on this route
+  // (including nested routes).
+  const authRequired = routeTo.matched.some((route) => route.meta.authRequired)
+  // If auth isn't required for the route, just continue.
+  if (!authRequired) return next()
 
-    // If auth isn't required for the route, just continue.
-    if (!authRequired) return next()
-
-    // If auth is required and the user is logged in...
-    if (store.getters['auth/loggedIn']) {
-      // Validate the local user token...
-      return store.dispatch('auth/validate').then((validUser) => {
-        // Then continue if the token still represents a valid user,
-        // otherwise redirect to login.
-        validUser ? next() : redirectToLogin()
-      })
-    }
-
-    // If auth is required and the user is NOT currently logged in,
-    // redirect to login.
-    redirectToLogin()
-
-    // eslint-disable-next-line no-unused-vars
-    // eslint-disable-next-line no-inner-declarations
-    function redirectToLogin() {
-      // Pass the original route to the login component
-      next({ name: 'login', query: { redirectFrom: routeTo.fullPath } })
-    }
+  // If auth is required and the user is logged in...
+  if (Vue.localStorage.get("token")) {
+    return next()
+    // Validate the local user token...
+    // return store.dispatch('auth/validate').then((validUser) => {
+    //   // Then continue if the token still represents a valid user,
+    //   // otherwise redirect to login.
+    //   validUser ? next() : redirectToLogin()
+    // })
   } else {
-    const publicPages = ['/login', '/register', '/forgot-password'];
-    const authpage = !publicPages.includes(routeTo.path);
-    const loggeduser = localStorage.getItem('user');
+    redirectToLogin()
+  }
 
-    if (authpage && !loggeduser) {
-      return next('/login');
-    }
+  // If auth is required and the user is NOT currently logged in,
+  // redirect to login.
+  redirectToLogin()
 
-    next();
+  // eslint-disable-next-line no-unused-vars
+  function redirectToLogin() {
+    // Pass the original route to the login component
+    next({ name: "login", query: { redirectFrom: routeTo.fullPath } })
   }
 })
 
@@ -92,7 +79,7 @@ router.beforeResolve(async (routeTo, routeFrom, next) => {
               // If redirecting to the same route we're coming from...
               // Complete the redirect.
               next(...args)
-              reject(new Error('Redirected'))
+              reject(new Error("Redirected"))
             } else {
               resolve()
             }
